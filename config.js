55a0ae6a4941ed20fe6c33fb1748080f1e61f947
segundocristian219-oss <segundocristian219@gmail.com>
2025-12-06 13:10:09 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,25 +3,25 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-   ['94949529878655', 'mau', true],
-    ['100644740407530', 'hernandez', true]
+   ['100644740407530', 'Hernandez', true],
+    ['227045091090524', 'erick', true]
 ]
 
 global.mods = []
 global.prems = []
 
-global.namebot = '𝙼𝚊𝚢𝚌𝚘𝚕𝐏𝐥𝐮𝐬'
+global.namebot = 'Hernandez𝐏𝐥𝐮𝐬'
 global.redes = 'https://chat.whatsapp.com/KDI7NNovzdwJayx1gI1cue?mode=ems_copy_t'
-global.botname = '𝕄𝕒𝕪𝕔𝕠𝕝𝐏𝐥𝐮𝐬'
+global.botname = 'Hernandez𝐏𝐥𝐮𝐬'
 global.banner = 'https://raw.githubusercontent.com/SoySapo6/tmp/refs/heads/main/Permanentes/images%20(8).jpeg'
 global.packname = '𝕄𝕒𝕪𝕔𝕠𝕝𝐏𝐥𝐮𝐬'
-global.author = '𝙃𝙚𝙘𝙝𝙤 𝙥𝙤𝙧 𝙎𝙤𝙮𝙈𝙖𝙮𝙘𝙤𝙡 <3'
-global.moneda = 'MayCoins'
+global.author = '𝙃𝙚𝙘𝙝𝙤 𝙥𝙤𝙧 𝙎𝙤𝙮Hernandez <3'
+global.moneda = 'BakCoins'
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.16'
 global.vs = '2.2.0'
-global.sessions = 'MayBot'
-global.jadi = 'MayBots'
+global.sessions = 'BakiBot'
+global.jadi = 'BakiBots'
 global.yukiJadibts = true
 
 global.namecanal = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
