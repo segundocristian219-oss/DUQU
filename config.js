0be2aa49e1b3b8a722e8481f11bdc22b62b9ba10
segundocristian219-oss <segundocristian219@gmail.com>
2025-12-06 13:00:05 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 global.owner = [
    ['94949529878655', 'mau', true],
-    ['237722815971456', 'cristian', true]
+    ['100644740407530', 'cristian', true]
 ]
 
 global.mods = []
